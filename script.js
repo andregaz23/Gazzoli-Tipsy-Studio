73c570a3e86afa2234eed14b350aa60ceadcfeba
andregaz23 <151537097+andregaz23@users.noreply.github.com>
2024-07-17 12:46:01 +0000

    prova slider

--- script.js
+++ script.js
@@ -16,3 +16,48 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 });
+ 
+
+document.addEventListener('DOMContentLoaded', function() {
+  const burgerMenu = document.querySelector('.burger-menu');
+  const navLinks = document.querySelector('.nav-links');
+  
+  burgerMenu.addEventListener('click', function() {
+    navLinks.classList.toggle('active');
+  });
+showSlides(slideIndex);
+});
+
+let slideIndex = 1;
+
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+let i;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+if (n > slides.length) {slideIndex = 1}    
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";  
+}
+for (i = 0; i < dots.length; i++) {
+  dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";  
+dots[slideIndex-1].className += " active";
+}
+
+
+let slideIndex2 = 1;
+
+
+function plusSlides2(n) {
+showSlides2(slideIndex2 += n);
+}
